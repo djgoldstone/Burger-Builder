3d399a78222a16506ad765db0d231b6a3b35e06f
Derek Goldstone <derekgoldstone@yahoo.com>
2019-07-04 10:32:46 -0700

    Added config directory with mysql schema

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Manly Burger');
+INSERT INTO burgers (burger_name) VALUES ('Lentil Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Lamb Burger');
